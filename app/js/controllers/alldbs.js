488672505f23d7acb765ecbc3c98aed165210cec
Jakob Westhoff <jakob@westhoffswelt.de>
2012-09-11 23:24:26 +0200

    Fixed:  strategie

--- app/js/controllers/alldbs.js
+++ app/js/controllers/alldbs.js
@@ -13,14 +13,14 @@ angular.module('pouchdemo.controllers')
 
         $scope.loadKnownDbs = function() {
             Pouch("idb://pouchdemo_metadata", function(err, db) {
-                db.get( "knownDbs", function(err, doc) {
-                    if ( err ) {
-                        $scope.knownDbs = null;
-                    } else {
-                        $scope.knownDbs = doc.knownDbs;
-                    }
-
-                    if (!$scope.$$phase) $scope.$digest();
+                $scope.$apply(function(){
+                    db.get( "knownDbs", function(err, doc) {
+                        if ( err ) {
+                            $scope.knownDbs = null;
+                        } else {
+                            $scope.knownDbs = doc.knownDbs;
+                        }
+                    });
                 });
             });
         }
@@ -28,13 +28,11 @@ angular.module('pouchdemo.controllers')
         $scope.showReplicationStatus = function( type, message ) {
             $scope.replicationStatusMessage = message;
             $scope.replicationStatusType = type;
-            if (!$scope.$$phase) $scope.$digest();
         }
 
         $scope.hideReplicationStatus = function() {
             $scope.replicationStatusMessage = null;
             $scope.replicationStatusType = null;
-            if (!$scope.$$phase) $scope.$digest();
         }
 
         $scope.replicateDb = function( dbmeta, fn ) {
@@ -44,25 +42,27 @@ angular.module('pouchdemo.controllers')
         $scope.addDbMeta = function( dbmeta, fn ) {
             Pouch("idb://pouchdemo_metadata", function(err, db) {
                 db.get( "knownDbs", function(err, doc) {
-                    if ( err ) {
-                        var knownDbs = {};
-                        knownDbs[dbmeta.name] = dbmeta;
+                    $scope.$apply(function(){
+                        if ( err ) {
+                            var knownDbs = {};
+                            knownDbs[dbmeta.name] = dbmeta;
+                            db.put({
+                                "_id": "knownDbs",
+                                "knownDbs": knownDbs
+                            }, fn);
+                            return;
+                        }
+
+                        if (  doc.knownDbs.constructor !== {}.constructor ) {
+                            doc.knownDbs = {};
+                        }
+                        doc.knownDbs[dbmeta.name] = dbmeta;
                         db.put({
                             "_id": "knownDbs",
-                            "knownDbs": knownDbs
+                            "_rev": doc._rev,
+                            "knownDbs": doc.knownDbs
                         }, fn);
-                        return;
-                    }
-
-                    if (  doc.knownDbs.constructor !== {}.constructor ) {
-                        doc.knownDbs = {};
-                    }
-                    doc.knownDbs[dbmeta.name] = dbmeta;
-                    db.put({
-                        "_id": "knownDbs",
-                        "_rev": doc._rev,
-                        "knownDbs": doc.knownDbs
-                    }, fn);
+                    });
                 });
             });
         }
@@ -85,17 +85,19 @@ angular.module('pouchdemo.controllers')
             }
 
             $scope.replicateDb( dbmeta, function( err, changes ) {
-                if (err) {
-                    $scope.showReplicationStatus( "error", "Error replicating: " + err.error );
-                    return;
-                }
-                $scope.addDbMeta( dbmeta, function( err, response ) {
+                $scope.$apply(function(){
                     if (err) {
-                        $scope.showReplicationStatus( "error", "Could not update PouchDemo metadb: " + err.error );
+                        $scope.showReplicationStatus( "error", "Error replicating: " + err.error );
                         return;
                     }
-                    $scope.loadKnownDbs();
-                    $scope.showReplicationStatus( "success", "Replicated successfully." );
+                    $scope.addDbMeta( dbmeta, function( err, response ) {
+                        if (err) {
+                            $scope.showReplicationStatus( "error", "Could not update PouchDemo metadb: " + err.error );
+                            return;
+                        }
+                        $scope.loadKnownDbs();
+                        $scope.showReplicationStatus( "success", "Replicated successfully." );
+                    });
                 });
             });
         }
